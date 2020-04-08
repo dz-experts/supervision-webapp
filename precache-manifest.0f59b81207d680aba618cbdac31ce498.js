3f912ad141a4bf5b0d28c8ddb5ed7b57e2b5c201
Taybou <benterki.tayeb@gmail.com>
2020-04-08 10:38:08 +0200

    Updates

---
+++ precache-manifest.0f59b81207d680aba618cbdac31ce498.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64cb5cf6a06d455e4d2786d50428a58d",
+    "url": "/supervision-webapp/index.html"
+  },
+  {
+    "revision": "62bdb9bcc3fa51530290",
+    "url": "/supervision-webapp/static/css/main.1317bc85.chunk.css"
+  },
+  {
+    "revision": "b2bc11338647301b4e9c",
+    "url": "/supervision-webapp/static/js/2.0a8edff1.chunk.js"
+  },
+  {
+    "revision": "5433e47b9de7fac7ec1519b892e28d67",
+    "url": "/supervision-webapp/static/js/2.0a8edff1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62bdb9bcc3fa51530290",
+    "url": "/supervision-webapp/static/js/main.aced6122.chunk.js"
+  },
+  {
+    "revision": "d68fb1aa1840b761565a",
+    "url": "/supervision-webapp/static/js/runtime-main.3481db65.js"
+  },
+  {
+    "revision": "f51d5f66ced0bd7af56861bd14ad7688",
+    "url": "/supervision-webapp/static/media/bg-01.f51d5f66.jpg"
+  },
+  {
+    "revision": "7e0487cc6ac4639c9e955b15f53d602b",
+    "url": "/supervision-webapp/static/media/covid-map.7e0487cc.jpg"
+  },
+  {
+    "revision": "d7d9110d3c10cfd71986e130892240fc",
+    "url": "/supervision-webapp/static/media/mask.d7d9110d.svg"
+  },
+  {
+    "revision": "f5e5b1880e7d63553e4c625696c70046",
+    "url": "/supervision-webapp/static/media/ventilator.f5e5b188.svg"
+  }
+]);
